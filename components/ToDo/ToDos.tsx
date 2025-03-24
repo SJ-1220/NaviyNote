abb202d9e0cd349f39cc445480bbae31a77a39c5
SJ-1220 <kindjin12@naver.com>
2025-03-24 19:53:56 +0900

    Feat: 로그인한 사용자 본인의 todo만 출력

--- components/ToDo/ToDos.tsx
+++ components/ToDo/ToDos.tsx
@@ -1,10 +1,11 @@
 'use client'
 import { supabase } from '@/lib/supabase'
+import { useSession } from 'next-auth/react'
 import { useEffect, useState } from 'react'
 
 interface Todo {
   id: string
-  user_id: string
+  user_email: string
   task: string
   completed: boolean
   date?: string
@@ -13,34 +14,38 @@ interface Todo {
 }
 
 export default function ToDos() {
-  const [data, setData] = useState<Todo[]>([])
+  const { data: session } = useSession()
+  const [todos, setTodos] = useState<Todo[]>([])
   const [loading, setLoading] = useState<boolean>(true)
   const [error, setError] = useState<string | null>(null)
 
   useEffect(() => {
     const fetchData = async () => {
-      const { data: todos, error } = await supabase.from('todo').select('*')
-      if (error) {
-        setError(error.message)
-      } else {
-        console.log(todos)
-        setData(todos)
+      if (session && session.user) {
+        const { data: fetchedTodos, error } = await supabase
+          .from('todo')
+          .select('*')
+          .eq('user_email', session.user.email)
+        if (error) {
+          setError(error.message)
+        } else {
+          setTodos(fetchedTodos)
+        }
       }
       setLoading(false)
     }
-
     fetchData()
-  }, [])
+  }, [session])
   if (loading) return <p>로딩 중</p>
   if (error) return <p>{error}</p>
   return (
     <div>
       <h1>Todo List</h1>
-      <ul>
-        {data.map((todo) => (
+      <div>
+        {todos.map((todo) => (
           <li key={todo.id}>
             <ol>todo id : {todo.id}</ol>
-            <ol>todo user-id : {todo.user_id}</ol>
+            <ol>todo user-id : {todo.user_email}</ol>
             <ol>task : {todo.task}</ol>
             <ol>completed : {todo.completed ? 'true' : 'false'}</ol>
             <ol>date : {todo.date}</ol>
@@ -48,7 +53,7 @@ export default function ToDos() {
             <ol>important : {todo.important ? 'true' : 'false'}</ol>
           </li>
         ))}
-      </ul>
+      </div>
     </div>
   )
 }
