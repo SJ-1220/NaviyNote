21f5bea32bf61d80a151bce172545f857855a687
SJ-1220 <kindjin12@naver.com>
2025-05-02 21:31:05 +0900

    Chore: 기본 route 사용 시, todolist 가져오기

--- components/ToDo/TodoModal.tsx
+++ components/ToDo/TodoModal.tsx
@@ -1,17 +1,46 @@
 'use client'
 import { useParams } from 'next/navigation'
-import React from 'react'
-import { Todo } from './todosServer'
+import React, { useEffect, useState } from 'react'
+import { fetchTodos, Todo } from './todosServer'
 import useTodoStore from '@/src/store/todoStore'
+import { useSession } from 'next-auth/react'
 
-const TodoModal = (todo: Todo) => {
+const TodoModal = () => {
   const { todoId } = useParams()
+  const { data: session } = useSession()
+  const [loading, setLoading] = useState(true)
   const todolist = useTodoStore((state) => state.todolist)
-  console.log('zustand에 저장된 todos', todolist)
+  const setTodosStore = useTodoStore((state) => state.setTodosStore)
+  // 기본 route에서는 todolist가 비어있어서 zustand에서 가져오기
+  useEffect(() => {
+    const fetchData = async () => {
+      if (todolist.length === 0 && session?.user?.email) {
+        try {
+          const fetchModalTodos = await fetchTodos(session.user.email)
+          setTodosStore(fetchModalTodos)
+        } catch (error) {
+          console.log(error)
+        }
+      }
+      setLoading(false)
+    }
+    fetchData()
+  }, [session, todolist.length, setTodosStore])
+  const todo = todolist.find((todo: Todo) => todo.id === todoId)
+  if (loading) {
+    return <div>Loading...</div>
+  }
+  if (!todo) {
+    return <div>Todo not found</div>
+  }
+  // console.log('zustand에 저장된 todos', todolist)
   return (
-    <div>
-      <div>TodoModal</div>
-      <div>{todoId}</div>
+    <div className="fixed inset-0 bg-black/20 flex justify-center items-center">
+      <div className="bg-black rounded-lg p-[4rem]">
+        <div>TodoModal</div>
+        <div>{todo.task}</div>
+        <div>{todo.id}</div>
+      </div>
     </div>
   )
 }
