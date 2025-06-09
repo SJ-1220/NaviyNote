4138deaf76b0c74842493b8a5983916ea82dffd9
SJ-1220 <kindjin12@naver.com>
2025-06-10 01:37:49 +0900

    Design: 친구, 통계 임시 page UI

--- components/Loading.tsx
+++ components/Loading.tsx
@@ -2,7 +2,7 @@ export default function LoadingPage() {
   return (
     <div className="text-center h-[20rem] items-center">
       <div className="text-[10rem]">◌</div>
-      <div>데이터가 로딩되고 있습니다</div>
+      <div className="text-[1.5rem]">데이터가 로딩되고 있습니다</div>
     </div>
   )
 }
--- src/app/(pages)/friend/page.tsx
+++ src/app/(pages)/friend/page.tsx
@@ -1,3 +1,18 @@
+import SoonImage from '../../../../public/images/coming-soon-image.png'
+import Image from 'next/image'
+
 export default function FriendPage() {
-  return <div>친구 페이지입니다</div>
+  return (
+    <div className="flex flex-col items-center justify-center h-[50rem]">
+      <div className="relative w-[40rem] h-[30rem]">
+        <Image
+          src={SoonImage}
+          alt="ComingSoonImage"
+          fill
+          style={{ objectFit: 'contain' }}
+        />
+      </div>
+      <div className="text-[1.5rem] font-bold">친구 페이지는 개발 중입니다</div>
+    </div>
+  )
 }
