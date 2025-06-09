34445a48c4698b2b70ab0fbcb57021c2407137bb
SJ-1220 <kindjin12@naver.com>
2025-06-09 20:37:17 +0900

    Design: Loading page UI

--- components/Loading.tsx
+++ components/Loading.tsx
@@ -1,3 +1,8 @@
 export default function LoadingPage() {
-  return <div className="text-center h-[60rem]">로딩중이에용</div>
+  return (
+    <div className="text-center h-[20rem] items-center">
+      <div className="text-[10rem]">◌</div>
+      <div>데이터가 로딩되고 있습니다</div>
+    </div>
+  )
 }
